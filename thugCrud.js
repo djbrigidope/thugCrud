31094dd0cc331abe9df1f15b4250649060926166
bj bautista <emailme@djbrigidope.com>
2017-09-19 21:50:05 -0700

    erased some terrible code

--- thugCrud.js
+++ thugCrud.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 // chmod u+x yourscript //to run w node
 //rap names for babies
-const name = process.argv[2];
-let thugNames = ['kyle','lil uzi vert', 'lil tunchy', 'thugnificent', ''];
-
-
-Array.prototype.read = function() {
-  this.forEach(function(name,index)){
-    console.log(name,index);
-  };
-};
-console.log('rap names for babies');
